3c98fa28c88b5bd6dbd8bb0743689a365cee2264
Martin Zackrisson <martin.zackrisson@gmail.com>
2016-10-14 17:46:23 +0200

    :star: Setup-script

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name="FlaskCamServer",
+      version='0.0',
+      description="Library for serving and recording cameras",
+      author="Martin Zackrisson",
+      author_email="martin.zackrisson@gmail.com",
+      packages=['flask_cam'])
