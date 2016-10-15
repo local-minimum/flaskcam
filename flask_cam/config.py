a46d80ddc5f14890f873e7319d462faa6013e73c
Martin Zackrisson <martin.zackrisson@gmail.com>
2016-10-15 16:49:48 +0200

    :wrench: Update config

--- flask_cam/config.py
+++ flask_cam/config.py
@@ -1,5 +1,6 @@
 tmp_path = "/dev/shm"
-tmp_names = "img_{request}_{sequence_length}.{suffix}"
+tmp_names = "FlaskCamServer_{request}_{sequence_length}.{suffix}"
 
 background_fps = 10
 background_request_id = 0
+background_cache_length = 10
