b46af131fc991f26335d33614441e9fc9f473219
Martin Zackrisson <martin.zackrisson@gmail.com>
2016-10-15 15:59:37 +0200

    :star: API for starting recording and stopping

--- flask_cam/server.py
+++ flask_cam/server.py
@@ -25,5 +25,31 @@ def get_app():
     app = Flask("FlaskCamServer")
 
     # TODO: Add routes
+    register_api(app)
 
     return app
+
+
+def register_api(app):
+
+    @app.route("/api/camera/on")
+    def _set_camera_on():
+        global __BACKGROUND_SERVICE
+        if __BACKGROUND_SERVICE is False:
+            t = Thread(target=background_service)
+            t.start()
+            return jsonify(success=True, is_endpoint=True)
+        return jsonify(success=False, is_endpoint=True, reason="Already running image acquisition")
+
+    @app.route("/api/camera/off")
+    def _set_camera_off():
+        global __BACKGROUND_SERVICE
+        if __BACKGROUND_SERVICE:
+            __BACKGROUND_SERVICE = False
+            return jsonify(success=True, is_endpoint=True)
+        return jsonify(success=False, is_endpoint=True, reason="Not running image acquisition")
+
+    @app.route("/api/image/recent")
+    def _get_most_resent_image():
+
+        return None
